7ed3087b3638b5b790a09731b63c86958e3ae88a
Jasmine Reyes <jasminereyess44@gmail.com>
2020-11-29 00:52:21 -0800

    details for vibes

--- src/OneVibe.js
+++ src/OneVibe.js
@@ -15,98 +15,6 @@ class OneVibe extends Component {
         }
     }
 
-    componentDidMount() {
-
-        let dropdown = document.getElementById('vibe-dropdown');
-        dropdown.length = 0;
-        let defaultOption = document.createElement('option');
-        defaultOption.text = "Choose a vibe playlist";
-        dropdown.add(defaultOption);
-        dropdown.selectedIndex = 0;
-
-        for (let i= 0; i < this.props.vibes.length; i++) {
-            let option = document.createElement('option');
-            option.text = this.props.vibes[i]
-            option.value = this.props.vibes[i]
-            option.id = this.props.vibes[i]
-            dropdown.add(option)
-        }
-
-
-        // for (let i= 0; i < this.props.vibes.length; i++) {
-        //     let vibe = this.props.vibes[i]
-        //     console.log(vibe)
-        //     let aArray = []
-        //     let sArray = []
-        //     fetch(`http://localhost:5000/journal-entries/${vibe}`)
-        //     .then(response => response.json())
-        //     .then(result => {
-        //         this.setState({
-        //             entry : result
-        //         })
-        //         console.log(vibe)
-        //         console.log(this.state.entry)
-
-        //         for (let i = 0; i < this.state.entry.length; i++) {
-        //             aArray.push(this.state.entry[i].song + " by " + this.state.entry[i].artist)
-        //             vibeMatches[vibe] = aArray
-        //         }
-        //         console.log("helloooooo")
-        //         // vibeMatches[vibe] = aArray
-        //         array.push(vibeMatches[vibe])
-        //         console.log(array)
-        //         allVibes.push(vibe)
-        //         console.log(allVibes)
-        //         this.setState({
-        //             songs : array,
-        //             vibes : allVibes
-        //         })
-
-        //         // console.log(vibeMatches)
-        //         // sArray.push(vibeMatches)
-        //         // console.log(sArray)
-
-        //         // this.setState({
-        //         //     matches : sArray
-        //         // })
-
-        //         // console.log((this.state.matches))
-
-
-        //     });   
-        // }
-    }
-
-    _clicked(){
-        let aArray = []
-        if(this.state.vibe !== "Choose a vibe playlist") {
-            fetch(`http://localhost:5000/journal-entries/${this.state.vibe}`)
-            .then(response => response.json())
-            .then(result => {
-                this.setState({
-                 entry : result
-                })
-
-                // console.log(vibe)
-                // console.log(this.state.entry)
-            
-                for (let i = 0; i < this.state.entry.length; i++) {
-                        aArray.push(this.state.entry[i].song + " by " + this.state.entry[i].artist)
-                }
-            });
-        }
-        this.setState({
-            songs : aArray
-        })
-    }
-
-    selectedVibe(event) {
-        console.log(event.target.value)
-        // this.setState({
-        //     vibe : event.target.value
-        // })
-        // console.log(this.state.vibe)
-    }
 
 
     render(){
@@ -114,9 +22,8 @@ class OneVibe extends Component {
         return(
             <div className = "listed-vibes">
                 <h2> vibessss </h2>
-                <select onChange = {this.selectedVibe} id= "vibe-dropdown"></select>
                 
-                <p> {this.props.vibes}</p>
+                <p> {this.props.song}</p>
             
             </div>
         )
--- src/Vibe.js
+++ src/Vibe.js
@@ -3,6 +3,23 @@ import './Vibe.css';
 import {withRouter} from "react-router";
 import OneVibe from './OneVibe'
 
+const emotions = {
+    "happy" : "😁",
+    "okay" : "🙂",
+    "neutral" : "😐",
+    "sad" : "🙁",
+    "cool" : "😎",
+    "awkward" : "🙃",
+    "ecstatic" : "🤩",
+    "smirky" : "😏",
+    "unfortunate" : "😪",
+    "worried" : "😟",
+    "overwhelmed" : "😭",
+    "defeated" : "😞",
+    "please" : "🥺"
+}
+let mango = [];
+let selected_vibes = [];
 class Vibe extends Component {
 
     constructor() {
@@ -11,19 +28,41 @@ class Vibe extends Component {
             entry : undefined,
             vibe_category : {},
             presentVibes : [],
-            presentSongs : []
+            presentSongs : [],
+            test :[],
+            hehe : []
         }
-        this.selectedVibe = this.selectedVibe.bind(this);
+        // this.selectedVibe = this.selectedVibe.bind(this);
     }
+
+//     selectedVibe(event){
+//         let array =[]
+//         console.log(event.target.value)
+//         let selection = event.target.value
+//         fetch(`http://localhost:5000/journal-entries/${selection}`)
+//             .then(response => response.json())
+//             .then(result => {
+//                 for(let i=0; i<result.length;i++) {
+//                     array.push(result[i].song + " by " + result[i].artist)
+//                 }
+
+//                 this.setState({
+//                     presentSongs : array
+//                 })
+//             });
+//             console.log(array)
+//         console.log(this.state.presentSongs)
+// }
 
     componentDidMount(){
 
-        let dropdown = document.getElementById('vibe-dropdown');
-        dropdown.length = 0;
-        let defaultOption = document.createElement('option');
-        defaultOption.text = "Choose a vibe playlist";
-        dropdown.add(defaultOption);
-        dropdown.selectedIndex = 0;
+        // let dropdown = document.getElementById('vibe-dropdown');
+        // let buttons = document.getElementById('buttons');
+        // dropdown.length = 0;
+        // let defaultOption = document.createElement('option');
+        // defaultOption.text = "Choose a vibe playlist";
+        // dropdown.add(defaultOption);
+        // dropdown.selectedIndex = 0;
 
 
         let p = [];
@@ -51,10 +90,58 @@ class Vibe extends Component {
 
         for (let i= 0; i < p.length; i++) {
             let option = document.createElement('option');
-            option.text = p[i]
-            option.value = p[i]
-            option.id = p[i]
-            dropdown.add(option)
+            let button = document.createElement('button')
+            button.innerHTML = emotions[p[i]]
+            button.id = p[i]
+            button.onclick = event => {
+                let selected_button = document.getElementById(event.target.id)
+                console.log(event.target.id)
+                let array =[]
+                let selection = event.target.id
+                if (selected_vibes.indexOf(event.target.id) === -1){
+                    selected_button.classList.add("show")
+                    selected_vibes.push(event.target.id)
+                }
+                else {
+                    for(let i= 0; i < selected_vibes.length; i++) {
+                        if(selected_vibes[i] === event.target.id) {
+                            selected_vibes.splice(i,1);
+                        }
+                    }
+        
+                    selected_button.classList.remove("show")
+                }
+
+                fetch(`http://localhost:5000/journal-entries/${selection}`)
+                    .then(response => response.json())
+                    .then(result => {
+                        for(let i=0; i<result.length;i++) {
+                            array.push(result[i].song + " by " + result[i].artist)
+
+                            if (selected_vibes.indexOf(selection)!== -1 && mango.indexOf(result[i].song + " by " + result[i].artist) === -1){
+                                mango.push(result[i].song + " by " + result[i].artist)
+                                console.log(mango)
+                            }
+                        //     else {
+                        //         for(let i= 0; i < mango.length; i++) {
+                        //             if (mango[i] === (result[i].song + " by " + result[i].artist)) {
+                        //                 selected_vibes.splice(i,1);
+                        //             }
+                        //     }
+                        // }
+
+                        this.setState({
+                            test : array,
+                            hehe : mango
+                        })
+                    }
+                });
+            }
+            // option.text = p[i]
+            // option.value = p[i]
+            // option.id = p[i]
+            // dropdown.add(option)
+            document.body.appendChild(button)
         }
 
             console.log(p)
@@ -63,27 +150,17 @@ class Vibe extends Component {
 
     }
 
-    selectedVibe(event){
-        let array =[]
-        console.log(event.target.value)
-        let selection = event.target.value
-        fetch(`http://localhost:5000/journal-entries/${selection}`)
-            .then(response => response.json())
-            .then(result => {
-                for(let i=0; i<result.length;i++) {
-                    array.push(result[i].song + " by " + result[i].artist)
-                }
-
-                this.setState({
-                    presentSongs : array
-                })
-            });
-            console.log(array)
-        console.log(this.state.presentSongs)
-}
 
 
     render(){
+        // let vibeComponent = ""
+        // if(this.state.test.length >0){
+        //     for(let i=0; i<this.state.test.length; i++){
+        //         vibeComponent = <OneVibe key = "test"
+        //                                  song = {this.state.test[i]}
+        //                         />
+        //     }
+        // }
 
         return (
 
@@ -93,9 +170,13 @@ class Vibe extends Component {
                         vibes = {p}
             />
                 <button onClick = {this.clickedVibes}> Vibes </button> */}
-                <p> present vibes +  {this.state.presentSongs[0]}</p>
-                <select onChange = {this.selectedVibe} id= "vibe-dropdown"></select>
-                <p> {this.state.presentSongs}</p>
+                {/* <p> present vibes +  {this.state.presentSongs[0]}</p>
+                <select onChange = {this.selectedVibe} id= "vibe-dropdown"></select> */}
+                <p> {this.state.test}</p>
+                <p> this is mango {this.state.hehe}</p>
+                <div id="buttons">
+
+                </div>
 
             </div>
         )
